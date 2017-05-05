a545de43500fa634da945b82e654b8f6326a34ee
LeslieKornes <Lesliekornes@gmail.com>
2017-05-05 15:44:21 -0400

    initial commit

---
+++ TravelReviewSite/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TravelReviewSite.Models
+{
+    public class Category
+    {
+        [Key]
+
+        public int ID { get; set; }
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+}
---
+++ TravelReviewSite/Models/Review.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelReviewSite.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Display(Name = "Date")]
+        [DisplayFormat(DataFormatString = "{0:M/d/yyyy}")]
+        public DateTime PublishedDate { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
